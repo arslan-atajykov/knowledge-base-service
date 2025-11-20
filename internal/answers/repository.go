ddf61000281cb788f03554d7c13ab43e7927967a
Arslan <trojanimax@gmail.com>
2025-11-21 00:40:44 +0300

    Временное in-memory хранилище

---
+++ internal/answers/repository.go
@@ -0,0 +1,69 @@
+package answers
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+type Repository struct {
+	mu      sync.RWMutex
+	answers map[uint]Answer
+	nextID  uint
+}
+
+func NewRepository() *Repository {
+	return &Repository{
+		answers: make(map[uint]Answer),
+		nextID:  1,
+	}
+}
+
+func (r *Repository) Create(questionID uint, userID, text string) Answer {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	a := Answer{
+		ID:         r.nextID,
+		QuestionID: questionID,
+		UserID:     userID,
+		Text:       text,
+		CreatedAt:  time.Now(),
+	}
+	r.answers[r.nextID] = a
+	r.nextID++
+	return a
+}
+
+func (r *Repository) Get(id uint) (Answer, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	a, ok := r.answers[id]
+	if !ok {
+		return Answer{}, errors.New("Не найдено")
+	}
+	return a, nil
+}
+
+func (r *Repository) Delete(id uint) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, ok := r.answers[id]; !ok {
+		return errors.New("Не найдено")
+	}
+	delete(r.answers, id)
+	return nil
+}
+
+func (r *Repository) GetByQuestion(questionID uint) []Answer {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	result := []Answer{}
+	for _, a := range r.answers {
+		if a.QuestionID == questionID {
+			result = append(result, a)
+		}
+	}
+}
---
+++ internal/questions/repository.go
@@ -0,0 +1,66 @@
+package questions
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+type Repository struct {
+	mu        sync.RWMutex
+	questions map[uint]Question
+	nextID    uint
+}
+
+func NewRepository() *Repository {
+	return &Repository{
+		questions: make(map[uint]Question),
+		nextID:    1,
+	}
+}
+
+func (r *Repository) Create(text string) Question {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	q := Question{
+		ID:        r.nextID,
+		Text:      text,
+		CreatedAt: time.Now(),
+	}
+	r.questions[r.nextID] = q
+	r.nextID++
+	return q
+}
+
+func (r *Repository) GetAll() []Question {
+	r.mu.RLock()
+	defer r.mu.Unlock()
+
+	results := make([]Question, 0, len(r.questions))
+
+	for _, q := range r.questions {
+		results = append(results, q)
+	}
+	return results
+}
+
+func (r *Repository) GetID(id uint) (Question, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	q, ok := r.questions[id]
+	if !ok {
+		return Question{}, errors.New("не найдено")
+	}
+	return q, nil
+}
+
+func (r *Repository) Delete(id uint) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, ok := r.questions[id]; !ok {
+		return errors.New("Не найдено")
+	}
+	delete(r.questions, id)
+	return nil
+}
