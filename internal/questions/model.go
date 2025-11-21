d01a0d65fd018c536cca8ed7310a00beba9a3164
Arslan <trojanimax@gmail.com>
2025-11-21 15:17:26 +0300

    Рабочая версия

--- internal/questions/model.go
+++ internal/questions/model.go
@@ -3,7 +3,7 @@ package questions
 import "time"
 
 type Question struct {
-	ID        uint      `json:"id"`
-	Text      string    `json:"text"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Text      string    `json:"text" gorm:"type:text;not null"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
